34394f34a224bcae4809e335492baf90b57c92ae
Eva <80333570+Evelin-yah@users.noreply.github.com>
2023-11-08 14:38:47 +0200

    new work

---
+++ object_and_classes_lab/zoo.py
@@ -0,0 +1,20 @@
+class Zoo:
+    __animals = 0
+    def __init__(self, name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animal(self, species, name):
+        pass
+
+    def get_info(self, species):
+        pass
+
+zoo_name = input()
+
+count = int(input())
+
+for animal in range(count):
+    pass
