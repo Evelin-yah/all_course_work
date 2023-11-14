117ce2084094dbcbb87b382aee32cf0cf2b6a83d
Eva <80333570+Evelin-yah@users.noreply.github.com>
2023-11-14 14:42:50 +0200

    adding new work

--- object_and_classes_lab/zoo.py
+++ object_and_classes_lab/zoo.py
@@ -7,14 +7,34 @@ class Zoo:
         self.birds = []
 
     def add_animal(self, species, name):
-        pass
+        if species == 'mammal':
+            self.mammals.append(name)
+        elif species == 'fish':
+            self.fishes.append(name)
+        elif species == 'bird':
+            self.birds.append(name)
+        Zoo.__animals += 1
 
     def get_info(self, species):
-        pass
+        string_for_print = ''
+        if species == 'mammal':
+            string_for_print += f"Mammals in {self.name}: {', '.join(self.mammals)}"
+        elif species == 'fish':
+            string_for_print += f"Fishes in {self.name}: {', '.join(self.fishes)}"
+        elif species == 'bird':
+            string_for_print += f"Birds in {self.name}: {', '.join(self.birds)}"
+        string_for_print += f'\nTotal animals: {Zoo.__animals}'
+        return string_for_print
 
 zoo_name = input()
+
+zoo_park = Zoo(zoo_name)
 
 count = int(input())
 
 for animal in range(count):
-    pass
+    species, name = input().split()
+    zoo_park.add_animal(species, name)
+
+searched_species = input()
+print(zoo_park.get_info(searched_species))
