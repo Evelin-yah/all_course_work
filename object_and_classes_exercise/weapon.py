34029b28a0cf08d875967c9dd35292b05dc4455f
Eva <80333570+Evelin-yah@users.noreply.github.com>
2023-11-15 12:32:15 +0200

    adding new work

---
+++ object_and_classes_exercise/weapon.py
@@ -0,0 +1,10 @@
+class Weapon:
+    bullets_count = 0
+    def __init__(self, bullets):
+        self.bullets = bullets
+
+    def shoot(self):
+        pass
+
+    def __repr__(self):
+        pass
